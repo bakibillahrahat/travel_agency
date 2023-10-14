139dbccb21fcb09b8b858c94a708f89c2d8b65e4
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-10-14 19:23:05 +0600

    complete Responsive Navigation for Travel Ageny

---
+++ components/index.tsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Camp from "./Camp";
+import Features from "./Features";
+import Footer from "./Footer";
+import GetApp from "./GetApp";
+import Guide from "./Guide";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export { Navbar, Button, Hero, Camp, Features, GetApp, Guide, Footer };
